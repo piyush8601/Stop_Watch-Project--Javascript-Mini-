bc2980e20777fd131c8e970f54a2ea559c6d9a9e
piyush8601 <115157297+piyush8601@users.noreply.github.com>
2023-07-15 15:39:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let timerDisplay = document.querySelector('.timer');
+let stopBtn = document.getElementById('stop-btn');
+let startBtn = document.getElementById('start-btn');
+let resetBtn = document.getElementById('reset-btn');
+
+let msec = 00;
+let secs = 00;
+let mins = 00;
+
+let timerId = null;
+
+startBtn.addEventListener('click',function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+
+});
+
+startBtn.addEventListener('click',function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click',function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click',function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 0;
+});
+
+function startTimer() {
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+            mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`
+}
